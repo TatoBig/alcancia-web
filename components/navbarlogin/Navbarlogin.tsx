0226e4947cead11cd78a18b4ef1cb432c6e29de2
tatobig <tatobig@gmail.com>
2023-04-28 09:55:49 +0000

    Added main page

--- components/navbarlogin/Navbarlogin.tsx
+++ components/navbarlogin/Navbarlogin.tsx
@@ -1,13 +1,16 @@
+import { useRouter } from "next/router"
 
 const Navbarlogin = () => {
+  const router = useRouter()
+
   return (
     <div className="flex justify-between w-100% h-[44px] bg-[#329F5B] md:pr-14">
       <div className=" md:justify-start text-white text-2xl font-bold  tracking-wide pl-4 ">
         <span className="inline-block align-middle drop-shadow-xl">Savings Vault</span>
       </div>
       <div>
-      <button className="text-white text-lg h-full px-6 font-semibold hover:bg-[#38BC7D] hover:drop-shadow-sm">Ingresar</button>
-      <button className="text-white text-lg h-full px-6 font-semibold hover:bg-[#38BC7D] hover:drop-shadow-sm">Registrarse</button>
+      <button onClick={() => router.push('/log-in')} className="text-white text-lg h-full px-6 font-semibold hover:bg-[#38BC7D] hover:drop-shadow-sm">Ingresar</button>
+      <button  onClick={() => router.push('/sign-in')} className="text-white text-lg h-full px-6 font-semibold hover:bg-[#38BC7D] hover:drop-shadow-sm">Registrarse</button>
       </div>
     </div>
   )
